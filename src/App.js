9bfe1f2cbb34b24c887f8a42ed9f854e5301a789
Navid Dezashibi <dezashibi@gmail.com>
2019-12-25 13:39:38 +0330

    moved store data to firebase, connected store data to firebase, updated redicer and selectors, create with-spinner HOC to be used with collection page component

--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
 
-
 class App extends React.Component {
 
   unsubscribeFromAuth = null
@@ -71,7 +70,7 @@ class App extends React.Component {
 }
 
 const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser
+  currentUser: selectCurrentUser,
 });
 
 const mapDispatchToProps = dispatch => ({
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -14,6 +14,8 @@ const config = {
     measurementId: "G-4F84D2HE5S"
 };
 
+firebase.initializeApp(config);
+
 export const createUserProfileDocument = async (userAuth, additionalData) => {
     if (!userAuth) return;
 
@@ -40,7 +42,36 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
     return userRef;
 };
 
-firebase.initializeApp(config);
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+    const collectionRef = firestore.collection(collectionKey);
+
+    const batch = firestore.batch();
+
+    objectsToAdd.forEach(obj => {
+        const newDocRef = collectionRef.doc();
+        batch.set(newDocRef, obj);
+    });
+
+    return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = (collections) => {
+    const transformedCollection = collections.docs.map(doc => {
+        const { title, items } = doc.data();
+
+        return {
+            routeName: encodeURI(title.toLowerCase()),
+            id: doc.id,
+            title,
+            items
+        }
+    });
+
+    return transformedCollection.reduce((accumulator, collection) => {
+        accumulator[collection.title.toLowerCase()] = collection;
+        return accumulator;
+    }, {});
+}
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
